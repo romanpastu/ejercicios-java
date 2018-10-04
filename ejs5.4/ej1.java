b68ceef9727f475cd7d3ec3e208c7ffb4d152ca6
Román Pastushenko <roman.pastushenko.1997@gmail.com>
2018-10-04 14:06:03 +0200

    update

---
+++ ejs5.4/ej1.java
@@ -0,0 +1,11 @@
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		for (int i = 0 ; i<=100; i+=5){
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ ejs5.4/ej2.java
@@ -0,0 +1,13 @@
+
+public class ej2 {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while (i <= 100){
+			System.out.println(i);
+			i += 5;	
+		}
+
+	}
+
+}
