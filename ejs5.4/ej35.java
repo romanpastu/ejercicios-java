a592aa977e2dcfd05e23e2b07ce5fc46776a8183
Román Pastushenko <roman.pastushenko.1997@gmail.com>
2018-10-18 11:11:06 +0200

    update

--- ejs5.4/ej35.java
+++ ejs5.4/ej35.java
@@ -17,68 +17,54 @@ public class ej35 {
 			if(fila > filaAuxiliar/2){
 			for(int j= 0; j< altura-fila; j++){
 					System.out.print(" ");
-			}
-			
+				}
 			}else{
-				for(int j = 0; j< fila-1; j++){ //fila = 6 j=0 //fila ultima = 1 <---- esto es para 13
+				for(int j = 0; j< fila-1; j++){ 
 					if(fila>=1){
 					System.out.print(" ");
 					}
-				}
-				
+				}	
 			}
-			
 			System.out.print("*");
 			
-			if(fila > filaAuxiliar/2){
-			
-						
+			if(fila > filaAuxiliar/2){		
 				if(fila == filaAuxiliar){
-					
 					for (int j = 0; j<filaAuxiliar-2; j++){
 						System.out.print(" ");
 					}
 					System.out.print("*");
 					
-				}else{
-					if(fila != (filaAuxiliar/2)+1){
+			}else{
+				if(fila != (filaAuxiliar/2)+1){
 					for ( int j = 0 ; j<filaAuxiliar-restadorAuxiliar;j++){
 						System.out.print(" ");
-						if(j == filaAuxiliar-restadorAuxiliar-1){
-							restadorAuxiliar+=2;
+							if(j == filaAuxiliar-restadorAuxiliar-1){
+								restadorAuxiliar+=2;
+							}
 						}
-					}
 					System.out.print("*");
 					}
-				}
-				
+				}	
 			}
 			
 		if(fila <= filaAuxiliar/2){	
-		if( fila == 1){
-			
-			for (int j = 0; j<filaAuxiliar-2; j++){
-				System.out.print(" ");
-			}
+			if( fila == 1){
+				for (int j = 0; j<filaAuxiliar-2; j++){
+					System.out.print(" ");
+				}
 			System.out.print("*");	
-		}else{
-			for(int j = 0; j< sumadorAuxiliar; j++){
-				System.out.print(" ");										//TODO 6 fila
-				
-			}
+			}else{
+				for(int j = 0; j< sumadorAuxiliar; j++){
+					System.out.print(" ");			
+				}
 			System.out.print("*");
 			sumadorAuxiliar+=2;
+			}
 		}
-		
-	}
-			
-			
-			
 			System.out.println();
 			fila--;
 		}	
-		
-		
+
 		}else{
 			System.out.println("Error, la atura es incorrecta, debe ser impar, mayor o igual que 3");
 		}
