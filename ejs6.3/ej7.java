dc4010a4893ec304d7621a1f680c6d0135a4d53c
Román Pastushenko <roman.pastushenko.1997@gmail.com>
2018-10-25 11:00:29 +0200

    update

---
+++ ejs6.3/ej7.java
@@ -0,0 +1,36 @@
+//*200 , 0-99 || *100+1 1-100
+public class ej7 {
+	public static void main(String[] args) {
+		
+		int numero1 = 1;
+		int numero2 = 2;
+		int numeroPartido = 1;
+		for (int i = 0; i<14;i++){
+			
+			System.out.printf("Nº de partido %2d : Equipo %2d vs Equipo %2d ",numeroPartido,numero1,numero2);
+			for(int j = 0; j<3;j++){
+				String resultado = "";
+				
+				int valor = ((int)(Math.random()*3+1));
+				switch (valor){
+				case 1:
+					resultado += "|1  |";
+					break;
+				case 2:
+					resultado += "| X |";
+					break;
+				case 3:
+					resultado += "|  2|";
+					break;
+				}	
+			System.out.printf("%s",resultado);
+			}
+			System.out.println();
+			numero1+=2;
+			numero2+=2;
+			numeroPartido++;
+		}
+
+	}
+
+}
