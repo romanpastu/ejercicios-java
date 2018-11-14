2bc3d75a5b3f911f48f07378f186938d78330baa
Román Pastushenko <roman.pastushenko.1997@gmail.com>
2018-11-14 14:14:19 +0100

    update

--- ejs7.4/ej15.java
+++ ejs7.4/ej15.java
@@ -22,43 +22,71 @@ public class ej15 {
 		System.out.println("¿Cuantos clientes sois?(-1 para salir)");
 
 		int numeroClientes = 0;
-		while (numeroClientes != -1) { //Por algun motivo es lo que hace que prosiga el loop
-			
+
+		while (numeroClientes != -1) { // Por algun motivo es lo que hace que
+										// prosiga el loop
+
 			do {
-				numeroClientes = S.nextInt();
-				if (numeroClientes > 4) {
-					System.out.println("Lo siento no admitimos grupos de " + numeroClientes
-							+ " haga grupos de 4 personas como maximo e intentelo de nuevo");
+
+				if (numeroClientes != -1) {
+					numeroClientes = S.nextInt();
+					if (numeroClientes > 4) {
+						System.out.println("Lo siento no admitimos grupos de " + numeroClientes
+								+ " haga grupos de 4 personas como maximo e intentelo de nuevo");
+					}
+				} else {
+					System.out.println("Has salido del programa");
+					break;
 				}
-				
-				if(numeroClientes == -1) {
-					System.exit(0);
-				}
+
 			} while (numeroClientes <= 0 || numeroClientes > 4);
 
-			//Esta es la situacion de si hay alguna mesa libre, con 0 comensales
-			for (int i = 0; i < mesas.length; i++) {
-				if (mesas[i] == 0) {
-					mesas[i] = numeroClientes;
-					System.out.println("Porfavor sientese en la mesa " + (i + 1));
-					i = mesas.length;
-				}
-			}
-			
-			
-			
-			//Esto se imprimira en cada pasada, los datos de las mesas
-			System.out.print("Mesa     ");
-			for (int i = 1; i <= 10; i++) {
-				System.out.printf(" %3d ", i);
-			}
-			System.out.println();
-			System.out.print("Ocupacion");
-			for (int i = 0; i < 10; i++) {
-				System.out.printf(" %3d ", mesas[i]);
-			}
-			System.out.println();
-		} 
-	}
+			// Esta es la situacion de si hay alguna mesa libre, con 0
+			// comensales
+			int mesasVacias = 0;
+			if (numeroClientes != -1) {
+				if (mesasVacias == 0) {
+					for (int i = 0; i < mesas.length; i++) {
 
+						if (mesas[i] == 0) {
+							mesas[i] = numeroClientes;
+							System.out.println("Porfavor sientese en la mesa " + (i + 1));
+							i = mesas.length;
+						}
+						if (i == mesas.length - 1) {
+							mesasVacias = 1;
+						}
+
+					}
+				}
+				// No quedan mesas vacias, hay que seguir aqui
+				if (mesasVacias != 0) {
+					System.out.println("No quedan mesas vacias");
+					for (int i = 0; i < mesas.length; i++) {
+						// Este es el caso en el que quepan en alguna mesa
+						if (4 - mesas[i] >= numeroClientes) {
+							mesas[i] += numeroClientes;
+							System.out.println();
+							System.out.println("Porfavor sientese en la mesa " + (i + 1) + " para compartir sitio");
+							i = mesas.length;
+						}
+
+					}
+
+				}
+				// Esto se imprimira en cada pasada, los datos de las mesas
+				System.out.print("Mesa     ");
+				for (int i = 1; i <= 10; i++) {
+					System.out.printf(" %3d ", i);
+				}
+				System.out.println();
+				System.out.print("Ocupacion");
+				for (int i = 0; i < 10; i++) {
+					System.out.printf(" %3d ", mesas[i]);
+				}
+				System.out.println();
+			}
+		}
+
+	}
 }
